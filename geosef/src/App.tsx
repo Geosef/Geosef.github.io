39c80070acac8da2d1296435cf89d27013f90928
Joe Carroll <jcarroll@resio.com>
2025-03-04 09:53:54 -0700

    initial pass at travel coordinator

---
+++ geosef/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import TravelCoordinator from './pages/TravelCoordinator';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Joe Carroll</h1>
+          <nav className="nav-bar">
+            <Link to="/travel-coordinator" className="nav-link">Travel Coordinator</Link>
+          </nav>
+        </header>
+        
+        <Routes>
+          <Route path="/travel-coordinator" element={<TravelCoordinator />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
